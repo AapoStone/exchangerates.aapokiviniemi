4b98cb08353b846abd76d40bf511fe84b4444b93
Aapo Kiviniemi <93884246+AapoStone@users.noreply.github.com>
2022-02-20 13:53:55 +0200

    API+Const+UI+Convert funktio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+const URL = "http://api.exchangeratesapi.io/v1/latest?access_key=";
+const API_KEY = "d2a384ce678331477a577dfd4b96f5f5";
 
 function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <form onSubmit={convert}>
+        <div>
+          <label>EUR</label>
+          <input
+            type="number"
+            step="0.01"
+            value={eur}
+            onChange={(e) => setEur(e.target.value)}
+          />
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>GBP</label>
+          <output>{gbp.toFixed(2)} £</output>
+        </div>
+        <div>
+          <button>Convert</button>
+        </div>
+      </form>
     </div>
   );
-}
 
+  async function convert(e) {
+    e.preventDefault();
+
+    const address = URL + API_KEY;
+    const response = await fetch(address);
+
+    if (response.ok) {
+      const json = await response.json();
+      setRate(json.rates.GBP);
+
+      setGbp(eur * json.rates.GBP);
+    } else {
+      alert("Error retrieving exchange rate.");
+    }
+  }
+}
 export default App;
